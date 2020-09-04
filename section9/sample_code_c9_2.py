555337de056f166ad4c2b324ab3516241603507d
wrist <stoicheia1986@gmail.com>
2020-09-05 01:08:42 +0900

    Modify c9_1 and c9_2 sample code, add c9_2.ipynb

--- section9/sample_code_c9_2.py
+++ section9/sample_code_c9_2.py
@@ -14,7 +14,7 @@ import itertools
 #Lh:残響除去フィルタのタップ長
 #return x_bar: 過去のマイク入力信号(Lh,M,Nk,Lt)
 def make_x_bar(x,D,Lh):
-    
+
     #フレーム数を取得
     Lt=np.shape(x)[2]
 
@@ -41,16 +41,16 @@ def dereverberation_ls(x,x_bar):
     x_bar=np.reshape(x_bar,[Lh*M,Nk,Lt])
     x_bar_x_bar_h=np.einsum('ikt,jkt->kij',x_bar,np.conjugate(x_bar))
     #covariance_inverse=np.linalg.pinv(x_bar_x_bar_h)
-    
+
     correlation=np.einsum('ikt,kt->ki',x_bar,np.conjugate(x[0,...]))
-    
+
     filter=np.linalg.solve(x_bar_x_bar_h,correlation)
 
     #filter=np.einsum('kij,kj->ki',covariance_inverse,correlation)
     x_reverb=np.einsum('kj,jkt->kt',np.conjugate(filter),x_bar)
 
     x_dereverb=x[0,...]-x_reverb
-        
+
     return(x_dereverb)
 
 #WPEで残響を除去
@@ -79,7 +79,7 @@ def dereverberation_wpe(x,x_bar,wpe_iterations=10):
 
         #フィルタ算出
         filter=np.linalg.solve(x_bar_x_bar_h,correlation)
-        
+
         #残響除去実施
         x_reverb=np.einsum('kj,jkt->kt',np.conjugate(filter),x_bar)
         x_dereverb=x[0,...]-x_reverb
@@ -134,169 +134,167 @@ def calculate_snr(desired,out):
 
     return(snr)
 
-#乱数の種を初期化
-np.random.seed(0)
+if __name__ == "__main__":
+    #乱数の種を初期化
+    np.random.seed(0)
 
-#畳み込みに用いる音声波形
-clean_wave_files=["./CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"]
+    #畳み込みに用いる音声波形
+    clean_wave_files=["./CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"]
 
-#音源数
-n_sources=len(clean_wave_files)
+    #音源数
+    n_sources=len(clean_wave_files)
 
-#長さを調べる
-n_samples=0
-#ファイルを読み込む
-for clean_wave_file in clean_wave_files:
-    wav=wave.open(clean_wave_file)
-    if n_samples<wav.getnframes():
-        n_samples=wav.getnframes()
-    wav.close()
+    #長さを調べる
+    n_samples=0
+    #ファイルを読み込む
+    for clean_wave_file in clean_wave_files:
+        wav=wave.open(clean_wave_file)
+        if n_samples<wav.getnframes():
+            n_samples=wav.getnframes()
+        wav.close()
 
-clean_data=np.zeros([n_sources,n_samples])
+    clean_data=np.zeros([n_sources,n_samples])
 
-#ファイルを読み込む
-s=0
-for clean_wave_file in clean_wave_files:
-    wav=wave.open(clean_wave_file)
-    data=wav.readframes(wav.getnframes())
-    data=np.frombuffer(data, dtype=np.int16)
-    data=data/np.iinfo(np.int16).max
-    clean_data[s,:wav.getnframes()]=data
-    wav.close()
-    s=s+1
+    #ファイルを読み込む
+    s=0
+    for clean_wave_file in clean_wave_files:
+        wav=wave.open(clean_wave_file)
+        data=wav.readframes(wav.getnframes())
+        data=np.frombuffer(data, dtype=np.int16)
+        data=data/np.iinfo(np.int16).max
+        clean_data[s,:wav.getnframes()]=data
+        wav.close()
+        s=s+1
+
+    # シミュレーションのパラメータ
+
+    #シミュレーションで用いる音源数
+    n_sim_sources=1
+
+    #サンプリング周波数
+    sample_rate=16000
+
+    #フレームサイズ
+    N=1024
+
+    #フレームシフト
+    Nshift=int(N/4)
+
+    #周波数の数
+    Nk=int(N/2+1)
+
+    #各ビンの周波数
+    freqs=np.arange(0,Nk,1)*sample_rate/N
+
+    #音声と雑音との比率 [dB]
+    SNR=90.
+
+    #部屋の大きさ
+    room_dim = np.r_[10.0, 10.0, 10.0]
+
+    #マイクロホンアレイを置く部屋の場所
+    mic_array_loc = room_dim / 2 + np.random.randn(3) * 0.1 
+
+    #マイクロホンアレイのマイク配置
+    mic_directions=np.array(
+        [[np.pi/2., theta/180.*np.pi] for theta in np.arange(180,361,180)
+        ]    )
+
+    distance=0.01
+    mic_alignments=np.zeros((3, mic_directions.shape[0]), dtype=mic_directions.dtype)
+    mic_alignments[0, :] = np.cos(mic_directions[:, 1]) * np.sin(mic_directions[:, 0])
+    mic_alignments[1, :] = np.sin(mic_directions[:, 1]) * np.sin(mic_directions[:, 0])
+    mic_alignments[2, :] = np.cos(mic_directions[:, 0])
+    mic_alignments *= distance
+
+    #マイクロホン数
+    n_channels=np.shape(mic_alignments)[1]
+
+    #マイクロホンアレイの座標
+    R=mic_alignments+mic_array_loc[:,None]
+
+    is_use_reverb=True
+    room = pa.ShoeBox(room_dim, fs=sample_rate, max_order=17,absorption=0.4)
+    room_no_reverb = pa.ShoeBox(room_dim, fs=sample_rate, max_order=0)
+
+    # 用いるマイクロホンアレイの情報を設定する
+    room.add_microphone_array(pa.MicrophoneArray(R, fs=room.fs))
+    room_no_reverb.add_microphone_array(pa.MicrophoneArray(R, fs=room.fs))
+
+    #音源の場所
+    doas=np.array(
+        [[np.pi/2., np.pi]
+        ]    )
+
+    #音源とマイクロホンの距離
+    distance=1.
+
+    source_locations=np.zeros((3, doas.shape[0]), dtype=doas.dtype)
+    source_locations[0, :] = np.cos(doas[:, 1]) * np.sin(doas[:, 0])
+    source_locations[1, :] = np.sin(doas[:, 1]) * np.sin(doas[:, 0])
+    source_locations[2, :] = np.cos(doas[:, 0])
+    source_locations *= distance
+    source_locations += mic_array_loc[:, None]
+
+    #各音源をシミュレーションに追加する
+    for s in range(n_sim_sources):
+        clean_data[s]/= np.std(clean_data[s])
+        room.add_source(source_locations[:, s], signal=clean_data[s])
+        room_no_reverb.add_source(source_locations[:, s], signal=clean_data[s])
 
 
-# シミュレーションのパラメータ
+    #シミュレーションを回す
+    room.simulate(snr=SNR)
+    room_no_reverb.simulate(snr=90)
 
-#シミュレーションで用いる音源数
-n_sim_sources=1
+    #畳み込んだ波形を取得する(チャンネル、サンプル）
+    multi_conv_data=room.mic_array.signals
+    multi_conv_data_no_reverb=room_no_reverb.mic_array.signals
 
-#サンプリング周波数
-sample_rate=16000
+    wave_len=np.shape(multi_conv_data_no_reverb)[1]
 
-#フレームサイズ
-N=1024
+    #畳み込んだ波形をファイルに書き込む
+    write_file_from_time_signal(multi_conv_data_no_reverb[0,:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_clean.wav",sample_rate)
 
-#フレームシフト
-Nshift=int(N/4)
+    #畳み込んだ波形をファイルに書き込む
+    write_file_from_time_signal(multi_conv_data[0,:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_in.wav",sample_rate)
 
+    #短時間フーリエ変換を行う
+    f,t,stft_data=sp.stft(multi_conv_data,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
 
-#周波数の数
-Nk=int(N/2+1)
+    #WPEの繰り返し回数
+    n_wpe_iterations=20
 
-#各ビンの周波数
-freqs=np.arange(0,Nk,1)*sample_rate/N
+    #残響除去のパラメータ
+    D=2
+    Lh=5
 
-#音声と雑音との比率 [dB]
-SNR=90.
+    #過去のマイクロホン入力信号
+    x_bar=make_x_bar(stft_data,D,Lh)
 
-#部屋の大きさ
-room_dim = np.r_[10.0, 10.0, 10.0]
+    #LSで残響除去
+    x_dereverb_ls=dereverberation_ls(stft_data,x_bar)
 
-#マイクロホンアレイを置く部屋の場所
-mic_array_loc = room_dim / 2 + np.random.randn(3) * 0.1 
+    #WPEで残響除去
+    x_dereverb_wpe,cost_buff_wpe=dereverberation_wpe(stft_data,x_bar,n_wpe_iterations)
 
-#マイクロホンアレイのマイク配置
-mic_directions=np.array(
-    [[np.pi/2., theta/180.*np.pi] for theta in np.arange(180,361,180)
-    ]    )
+    #x:入力信号( M, Nk, Lt)
 
-distance=0.01
-mic_alignments=np.zeros((3, mic_directions.shape[0]), dtype=mic_directions.dtype)
-mic_alignments[0, :] = np.cos(mic_directions[:, 1]) * np.sin(mic_directions[:, 0])
-mic_alignments[1, :] = np.sin(mic_directions[:, 1]) * np.sin(mic_directions[:, 0])
-mic_alignments[2, :] = np.cos(mic_directions[:, 0])
-mic_alignments *= distance
+    t,x_dereverb_ls=sp.istft(x_dereverb_ls,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
+    t,x_dereverb_wpe=sp.istft(x_dereverb_wpe,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
 
-#マイクロホン数
-n_channels=np.shape(mic_alignments)[1]
+    snr_pre=calculate_snr(multi_conv_data_no_reverb[0,...],multi_conv_data[0,...])
+    snr_ls_post=calculate_snr(multi_conv_data_no_reverb[0,...],x_dereverb_ls)
+    snr_wpe_post=calculate_snr(multi_conv_data_no_reverb[0,...],x_dereverb_wpe)
 
-#マイクロホンアレイの座標
-R=mic_alignments+mic_array_loc[:,None]
+    write_file_from_time_signal(x_dereverb_ls[:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_ls_{}_{}.wav".format(Lh,D),sample_rate)
+    write_file_from_time_signal(x_dereverb_wpe[:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_wpe_{}_{}.wav".format(Lh,D),sample_rate)
 
-is_use_reverb=True
-room = pa.ShoeBox(room_dim, fs=sample_rate, max_order=17,absorption=0.4)
-room_no_reverb = pa.ShoeBox(room_dim, fs=sample_rate, max_order=0)
+    print("method:    ", "LS","WPE")
+    print("Δsnr [dB]: {:.2f}  {:.2f}".format(snr_ls_post-snr_pre,snr_wpe_post-snr_pre))
 
-# 用いるマイクロホンアレイの情報を設定する
-room.add_microphone_array(pa.MicrophoneArray(R, fs=room.fs))
-room_no_reverb.add_microphone_array(pa.MicrophoneArray(R, fs=room.fs))
+    #コストの値を表示
+    #for t in range(n_wpe_iterations):
+    #    print(t,cost_buff_wpe[t])
 
-#音源の場所
-doas=np.array(
-    [[np.pi/2., np.pi]
-    ]    )
-
-#音源とマイクロホンの距離
-distance=1.
-
-source_locations=np.zeros((3, doas.shape[0]), dtype=doas.dtype)
-source_locations[0, :] = np.cos(doas[:, 1]) * np.sin(doas[:, 0])
-source_locations[1, :] = np.sin(doas[:, 1]) * np.sin(doas[:, 0])
-source_locations[2, :] = np.cos(doas[:, 0])
-source_locations *= distance
-source_locations += mic_array_loc[:, None]
-
-#各音源をシミュレーションに追加する
-for s in range(n_sim_sources):
-    clean_data[s]/= np.std(clean_data[s])
-    room.add_source(source_locations[:, s], signal=clean_data[s])
-    room_no_reverb.add_source(source_locations[:, s], signal=clean_data[s])
     
-
-#シミュレーションを回す
-room.simulate(snr=SNR)
-room_no_reverb.simulate(snr=90)
-
-#畳み込んだ波形を取得する(チャンネル、サンプル）
-multi_conv_data=room.mic_array.signals
-multi_conv_data_no_reverb=room_no_reverb.mic_array.signals
-
-wave_len=np.shape(multi_conv_data_no_reverb)[1]
-
-#畳み込んだ波形をファイルに書き込む
-write_file_from_time_signal(multi_conv_data_no_reverb[0,:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_clean.wav",sample_rate)
-
-#畳み込んだ波形をファイルに書き込む
-write_file_from_time_signal(multi_conv_data[0,:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_in.wav",sample_rate)
-
-#短時間フーリエ変換を行う
-f,t,stft_data=sp.stft(multi_conv_data,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
-
-#WPEの繰り返し回数
-n_wpe_iterations=20
-
-#残響除去のパラメータ
-D=2
-Lh=5
-
-#過去のマイクロホン入力信号
-x_bar=make_x_bar(stft_data,D,Lh)
-
-#LSで残響除去
-x_dereverb_ls=dereverberation_ls(stft_data,x_bar)
-
-#WPEで残響除去
-x_dereverb_wpe,cost_buff_wpe=dereverberation_wpe(stft_data,x_bar,n_wpe_iterations)
-
-#x:入力信号( M, Nk, Lt)
-
-t,x_dereverb_ls=sp.istft(x_dereverb_ls,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
-t,x_dereverb_wpe=sp.istft(x_dereverb_wpe,fs=sample_rate,window="hann",nperseg=N,noverlap=N-Nshift)
-
-snr_pre=calculate_snr(multi_conv_data_no_reverb[0,...],multi_conv_data[0,...])
-snr_ls_post=calculate_snr(multi_conv_data_no_reverb[0,...],x_dereverb_ls)
-snr_wpe_post=calculate_snr(multi_conv_data_no_reverb[0,...],x_dereverb_wpe)
-
-write_file_from_time_signal(x_dereverb_ls[:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_ls_{}_{}.wav".format(Lh,D),sample_rate)
-write_file_from_time_signal(x_dereverb_wpe[:wave_len]*np.iinfo(np.int16).max/20.,"./dereverb_wpe_{}_{}.wav".format(Lh,D),sample_rate)
-
-
-
-print("method:    ", "LS","WPE")
-print("Δsnr [dB]: {:.2f}  {:.2f}".format(snr_ls_post-snr_pre,snr_wpe_post-snr_pre))
-
-#コストの値を表示
-#for t in range(n_wpe_iterations):
-#    print(t,cost_buff_wpe[t])
-
